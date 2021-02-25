d14d4f96b92e9e5a810b9c2ab9c2735404d1d837
muhammadlovlu <chyshahab@gmail.com>
2021-02-25 07:19:55 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -19,22 +19,59 @@ const displayAllSongs = songs => {
         const songListing = `
        <div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9">
-                        <h3 class="lyrics-name">${song.title}</h3>
+                        <h3 id="song-title" class="lyrics-name">${song.title}</h3>
                         <audio controls>
                         <source src="${song.preview}" type="audio/mpeg">
                       Your browser does not support the audio element.
                       </audio>
 
 
-                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <p class="author lead">Album by <span id="artist">${song.artist.name}</span></p>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick="FindLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
                     </div>
                 </div> 
        `;
         div.innerHTML = songListing;
         listing.appendChild(div)
     });
+
 }
+
+
+
+
+
+
+async function FindLyrics(artist, title) {
+    const artistName = artist;
+    const songTitle = title;
+    const api = `https://api.lyrics.ovh/v1/${artistName}/${songTitle}`
+    const res = await fetch(api);
+    const data = await res.json();
+    lyrics(data);
+
+}
+
+
+
+const lyrics = data => {
+
+ const lyrics = document.getElementById("lyrics");
+ const p = document.createElement("p");
+ p.innerText = data.lyrics;
+ lyrics.appendChild(p);
+}
+
+
+
+
+
+
+
+
+
+
+
 
